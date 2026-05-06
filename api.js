6703fc4f563070295ba4aae071c508219f758089
Joao Hipolito <joaopedrohipolito.2004@gmail.com>
2026-05-06 10:54:13 +0100

    update das paginas de api.js e index.html

--- api.js
+++ api.js
@@ -7,20 +7,11 @@ const BASE_URL = 'https://api.rawg.io/api/';
 async function fetchGames(endpoint, params = "") {
     try {
         const response = await fetch(`${BASE_URL}${endpoint}?key=${API_KEY}${params}`);
-        
-        if (!response.ok) {
-            throw new Error(`Erro na API: ${response.status}`);
-        }
-
+        if (!response.ok) throw new Error(`Erro: ${response.status}`);
         const data = await response.json();
-        
-        // Se for um detalhe único, retorna o objeto; se for lista, retorna o array results
         return data.results ? data.results : data;
     } catch (error) {
-        console.error("Erro ao procurar dados:", error);
-        // Exemplo de Tratamento de Erros (Requisito)
-        const container = document.querySelector('.view:not([style*="none"])');
-        container.innerHTML = `<p class="error">Pedimos desculpa, ocorreu um erro ao carregar os jogos.</p>`;
+        console.error("Erro na API:", error);
         return [];
     }
 }
